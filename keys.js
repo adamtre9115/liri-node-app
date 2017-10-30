e38ad4b51b8675f76b6af8568ecd049462a53a4f
adamtre9115 <treadam9115@gmail.com>
2017-10-30 18:07:47 -0400

    initial commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'pNDwqNif14BYHYwDeLssBPiu0',
+  consumer_secret: '88LcGJMrb8OhQYOp1nekKe4rPujz9VdmJVSbQglW0cFyL7rvO1',
+  access_token_key: '223348242-jTpWmPYLz85jAJwFvWEdeyT4ihxnlg29ANUMSzHk',
+  access_token_secret: '6STU88aNz1lPSoNDMZ35PApseVG3qwDUFINjIIdJENofO',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,3 @@
+//variable for keys
+var keys = require("keys.js");
+var Twitter = require('twitter');
